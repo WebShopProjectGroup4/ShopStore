f59eb5325094a9a2a7dc6975f137cbf7289fc337
Kantha-K <101991928+Kantha-K@users.noreply.github.com>
2022-06-09 16:09:24 +0200

    Added reviews page

--- webshop/shopApp/models.py
+++ webshop/shopApp/models.py
@@ -1,5 +1,8 @@
+from operator import mod
 from django.db import models
 from django.urls import reverse
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -45,3 +48,16 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product_detail',
                        args=[self.id,self.slug])
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True) 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    review = models.TextField(max_length=1000,blank=True)
+    title = models.CharField(max_length=100,blank=True)
+    rating = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return " Review: " + str(self.title) 
