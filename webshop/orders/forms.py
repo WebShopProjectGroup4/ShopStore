08dc7c88129f8a3cb52518cdc971b05854f863e5
Kantha-K <101991928+Kantha-K@users.noreply.github.com>
2022-06-14 20:57:09 +0200

    Added Css for checkout,cart

--- webshop/orders/forms.py
+++ webshop/orders/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 from .models import Order
 class OrderCreateForm(forms.ModelForm):
+
+    email=forms.EmailField()
+    first_name=forms.CharField()
+    last_name=forms.CharField()
+    address=forms.CharField()
+    postal_code=forms.CharField()
+    city=forms.CharField()
+
+    email.widget.attrs.update({'class':'form-control w-50 '})
+    first_name.widget.attrs.update({'class':'form-control w-50'})
+    last_name.widget.attrs.update({'class':'form-control w-50'})
+    address.widget.attrs.update({'class':'form-control w-50'})
+    postal_code.widget.attrs.update({'class':'form-control w-50'})
+    city.widget.attrs.update({'class':'form-control w-50'})
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address',
